588e6fa164bb049fa8ff162f82031cb16fa1178d
TamasKrisztian <kortanaz@gmail.com>
2020-12-10 09:39:13 +0100

    project scope és feladat lépéseinek meghatározása

---
+++ script.js
@@ -0,0 +1,35 @@
+function pageLoad() {
+/*todo appot csinálunk alapértelmezésben 10 üres mező áll rendelkezésre
+ de hozzátudunk adni újakat és minden sorban van egy checkbox textinput mező feladat leírásával
+és a egy törlés gomb amivel tudjuk a sort törölni */
+
+// Létrehozok egy változót a root-nak
+let root = document.getElementById("root");
+
+//	El kell tárolnunk egy változóba a sorok számát és a sornak a html struktúráját(div checkbox text input button)
+let rowNumber = 10
+
+function createRow() {
+	root.insertAdjacentHTML
+}
+
+// 10* kell duplikálni az előző kommentben meghatározott sort for ciklussal és hozzáadjuk a roothoz
+
+
+// rooton kívülre be kell rakni egy + gombot amivel hozzáadhatunk üres sorokat
+
+
+// Hozzáadunk egy click eseményt a + gombhoz 
+
+
+// Kijelölölm class név alapján az összes törlés gombot és elmentem egy változóba
+
+
+// click esemény adása for ciklussal a törlés gombokra
+
+
+
+}
+
+window.addEventListener("load", pageLoad);
+
